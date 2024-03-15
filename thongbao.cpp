a10d3091b6680da2b82d90a5df5f46e844afc758
nvcan1236 <ngcanh1236@gmail.com>
2024-03-15 16:15:20 +0700

    76_NguyenVanCanh_buoi2 them dong file thongbao

--- thongbao.cpp
+++ thongbao.cpp
@@ -5,5 +5,8 @@ int main()
 {
 	cout << "Stt 39 Xin chao 76";
 	cout << "76_NguyenVanCanh nhanh moi Xin chao !!!";
+	cout << "76_NguyenVanCanh chinh sua nha !!!";
+
+	cout << "76_NguyenVanCanh chinh sua de tao Conflic !!!";
 	return 0;
 }
